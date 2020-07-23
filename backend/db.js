b415a59b4208572627b6ad4e7a2f7d8d48d5fb24
Md Shahazad Uddin <shanudjn@gmail.com>
2020-07-23 21:25:43 +0530

    added nodemon and connected to mongodb server

--- backend/db.js
+++ backend/db.js
@@ -1,2 +1,15 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/postManagerDB");
+
+mongoose.connect(
+  "mongodb://localhost:27017/postManagerDB",
+  { useNewUrlParser: true },
+  (err) => {
+    if (!err) {
+      console.log("Connection to db successful");
+    } else {
+      console.log(
+        "Error while connecting to database" + JSON.stringify(err, undefined, 2)
+      );
+    }
+  }
+);

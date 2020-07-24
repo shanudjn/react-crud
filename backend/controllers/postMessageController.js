0e92dd503517151cca821dee015379ee4f84cee0
Md Shahazad Uddin <shanudjn@gmail.com>
2020-07-24 23:37:22 +0530

    added post and put endpoints

--- backend/controllers/postMessageController.js
+++ backend/controllers/postMessageController.js
@@ -13,4 +13,35 @@ router.get("/", (req, res) => {
   });
 });
 
+router.post("/", (req, res) => {
+  let newRecord = {
+    title: req.body.title,
+    message: req.body.message,
+  };
+  newRecord.save((err, docs) => {
+    if (!err) res.send(docs);
+    else
+      console.log(
+        "error while retirving all records" + JSON.stringify(err, undefined, 2)
+      );
+  });
+});
+
+router.put("/:id", (req, res) => {
+  let newRecord = {
+    title: req.body.title,
+    message: req.body.message,
+  };
+
+  postMessage.findOneAndUpdate(
+    req.params.id,{ $set: newRecord },(err, docs) => {
+      if (!err) res.send(docs);
+      else
+        console.log(
+          "error while retirving all records" +
+            JSON.stringify(err, undefined, 2)
+        );
+    }
+  );
+});
 module.exports = router;

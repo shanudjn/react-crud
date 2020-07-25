53a7cc6f81ca66bdc62396870df80ca545292aab
Md Shahazad Uddin <shanudjn@gmail.com>
2020-07-25 13:47:26 +0530

    added the put and delete endpoints

--- backend/controllers/postMessageController.js
+++ backend/controllers/postMessageController.js
@@ -1,7 +1,9 @@
 const express = require("express");
 let router = express.Router();
+var ObjectID = require('mongoose').Types.ObjectId
 
 var { PostMessage } = require("../models/postMessage");
+const { ObjectId } = require("mongodb");
 
 router.get("/", (req, res) => {
   PostMessage.find((err, doc) => {
@@ -14,10 +16,10 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  let newRecord = {
+  let newRecord = new PostMessage({
     title: req.body.title,
     message: req.body.message,
-  };
+  });
   newRecord.save((err, docs) => {
     if (!err) res.send(docs);
     else
@@ -28,20 +30,30 @@ router.post("/", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-  let newRecord = {
+  if (!ObjectId.isValid(req.params.id)) {
+    return res.status(400).send('No record with given id' + req.params.id);
+  }
+
+  let newRecord = new PostMessage({
     title: req.body.title,
     message: req.body.message,
-  };
+  });
 
   postMessage.findOneAndUpdate(
-    req.params.id,{ $set: newRecord },(err, docs) => {
+    req.params.id, { $set: newRecord }, (err, docs) => {
       if (!err) res.send(docs);
-      else
-        console.log(
-          "error while retirving all records" +
-            JSON.stringify(err, undefined, 2)
-        );
-    }
-  );
+      else console.log("error while retirving all records" + JSON.stringify(err, undefined, 2))
+    });
 });
+
+router.delete('/:id', (req, docs) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    return res.status(400).send('No record with given id' + req.params.id);
+  }
+  postMessage.findByIdAndRemove(req.params.id, (err, docs) => {
+    if (!err) res.send(docs)
+    else console.log('error while deleting a record: ' + JSON.stringify(err, undefined, 2))
+  });
+})
+
 module.exports = router;
--- backend/models/postMessage.js
+++ backend/models/postMessage.js
@@ -4,6 +4,6 @@ const mongoose = require("mongoose");
 var PostMessage = mongoose.model("PostMessage", {
   title: String,
   message: String,
-});
+}, 'postMessages');
 
 module.exports = { PostMessage };

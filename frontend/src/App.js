b3e67f409980fec6ca03540f8450da41253e502d
Md Shahazad Uddin <shanudjn@gmail.com>
2020-08-08 19:00:48 +0530

    added form bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-
 import './App.css';
 import PostMessages from './components/PostMessages';
 import PostMessagesForm from './components/PostMessageForm';
 
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
-      <PostMessages />
       <PostMessagesForm />
+      <PostMessages />
     </div>
   );
 }
--- frontend/src/components/PostMessageForm.js
+++ frontend/src/components/PostMessageForm.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+
 const PostMessagesForm = () => {
     return (
-        <div>
-            <h1>I am the form</h1>
+        <div className='form'>
+            <Form>
+                <FormGroup>
+                    <Input type="text" name="title" id="title" placeholder="Title" />
+                </FormGroup>
+                <FormGroup>
+                    <Input type="textarea" name="text" id="exampleText" placeholder="Message" />
+                </FormGroup>
+
+                <Button>Submit</Button>
+            </Form>
         </div>
+
     )
 }
 
